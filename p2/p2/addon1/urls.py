cc2e4d19abe99b854233fd11c1f47bde7b8d9843
baao090426hmcrvsa5-lab <baao090426hmcrvsa5@soycecytem.mx>
2025-11-13 20:52:40 +0000

    1

---
+++ p2/p2/addon1/urls.py
@@ -0,0 +1,7 @@
+From django.urls import path from import views
+
+urlpatterns[
+
+path("", views.home, name="addon1_home"),
+
+1
---
+++ p2/p2/addon1/views.py
@@ -0,0 +1,7 @@
+
+From django.shortcuts import render
+
+def home(request):
+
+return render(request, "addon1/home.html")
+# Create your views here.
